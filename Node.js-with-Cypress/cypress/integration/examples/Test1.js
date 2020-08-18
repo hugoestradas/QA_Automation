e9f9dd159d06842ff062eca2ae6271437da1d2f1
Hugo Estrada S <61108175+hugoestradas@users.noreply.github.com>
2020-08-18 16:03:53 -0600

    automate how to add product to car

--- Node.js-with-Cypress/cypress/integration/examples/Test1.js
+++ Node.js-with-Cypress/cypress/integration/examples/Test1.js
@@ -1,8 +1,16 @@
+// <reference types="Cypress" />
+
 describe('My first test suite', function (){
     it('My first test case', function(){
         cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
         cy.get('.search-keyword').type('ca')
         cy.wait(2000)
+        cy.get('.product').should('have.length',5)
         cy.get('.product:visible').should('have.length', 4)
+        //parent child chaining
+        cy.get('.products').find('.product').should('have.length', 4)
+        cy.get(':nth-child(3) > .product-action > button').click()
+        cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+
     })
 })
