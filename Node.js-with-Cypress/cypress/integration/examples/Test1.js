d23e21fe87efb560165f1ce4911333cd5436bffa
Hugo Estrada S <61108175+hugoestradas@users.noreply.github.com>
2020-08-18 12:41:49 -0600

    assertion in writing tests

--- Node.js-with-Cypress/cypress/integration/examples/Test1.js
+++ Node.js-with-Cypress/cypress/integration/examples/Test1.js
@@ -1,5 +1,11 @@
+// <reference types="Cypress" />
+
 describe('My first test suite', function (){
     it('My first test case', function(){
-        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
-    }) 
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+        cy.get('.search-keyword').type('ca')
+        cy.wait(2000)
+        //cy.get('.product').should('have.length',4)
+        cy.get('.product:visible').should('have.length', 4)
+    })
 })
