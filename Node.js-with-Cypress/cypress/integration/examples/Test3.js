f24dbe3cfed89287d1a7eca804b23ee06c073c32
Hugo Estrada S <61108175+hugoestradas@users.noreply.github.com>
2020-08-19 17:52:36 -0600

    handling dynamic dropdowns using select command

--- Node.js-with-Cypress/cypress/integration/examples/Test3.js
+++ Node.js-with-Cypress/cypress/integration/examples/Test3.js
@@ -8,5 +8,15 @@ describe('My Second Test Suite', function() {
 
         //static dropdown
         cy.get('select').select('option2').should('have.value','option2')
+
+        //dynamic dropdowns
+        cy.get('#autocomplete').type('ind')
+
+        cy.get('.ui-menu-item div').each(($e1, index, $list) =>{
+            if($e1.text()==='India'){
+                $e1.click()
+            }
+        })
+
     })
 })
