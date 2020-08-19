305138ab12a01dd8d13bcafddfe328ad44131993
Hugo Estrada S <61108175+hugoestradas@users.noreply.github.com>
2020-08-19 14:51:44 -0600

    adding multiple checkboxes

---
+++ Node.js-with-Cypress/cypress/integration/examples/Test3.js
@@ -0,0 +1,9 @@
+describe('My Second Test Suite', function() {
+    it('My FirstTest case',function() {
+        cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+        cy.get('#checkBoxOption1').check().should('be.checked').and('have.value', 'option1')
+        cy.get('#checkBoxOption1').uncheck().should('not.be.checked')
+        // select multiple check boxes
+        cy.get('input[type="checkbox"]').check(['option2', 'option3'])
+    })
+})
