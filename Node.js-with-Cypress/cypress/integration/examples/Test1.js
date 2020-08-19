93bb05ccdf5c65d600ce7e114df1e7cbc3d54fe5
Hugo Estrada S <61108175+hugoestradas@users.noreply.github.com>
2020-08-19 10:43:57 -0600

    grabbing the text for validations

--- Node.js-with-Cypress/cypress/integration/examples/Test1.js
+++ Node.js-with-Cypress/cypress/integration/examples/Test1.js
@@ -11,6 +11,12 @@ describe('My first test suite', function (){
         cy.get('.products').find('.product').should('have.length', 4)
         cy.get(':nth-child(3) > .product-action > button').click()
         cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+        cy.get('.products').find('.product').each(($el, index, $list) => {
+            const textVeg = $el.find('h4.product-name').text()
+            if(textVeg.includes('Cashews')){
+                $el.find('button').click()
+            }
+        })
 
     })
 })
