81b1e9198f219aa5b3cfc4c60cd3f38a4f370ba7
Hugo Estrada S <61108175+hugoestradas@users.noreply.github.com>
2020-08-20 00:32:39 -0600

    Update demo.js

--- Node.js-with-Cypress/cypress/integration/examples/demo.js
+++ Node.js-with-Cypress/cypress/integration/examples/demo.js
@@ -5,5 +5,6 @@ describe("Frames Test", function (){
         cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
         cy.frameLoaded('#courses-iframe')
         cy.iframe().find("a[href*='mentorship']").eq(0).click()
+        cy.iframe().find("h1[class*='pricing-title']").should('have.length',2)
     })
 })
